35fd508c8a2739ab019d5cdeed236d9a6907707e
Adarsh <adarshs8400@gmail.com>
2026-03-31 13:29:23 +0530

    created update form

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.list_blogs, name='blog_list'),
     path('/blog-detail/<int:pk>', views.blog_detail, name='blog_detail'),
     path('/create-blog', views.blog_create, name='blog_create'),
+    path('/update-blog/<int:pk>', views.update_blog, name='blog_update'),
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -23,3 +23,21 @@ def blog_create(request):
             form.save()
             return redirect('/')
     return render(request, 'blog_form.html', {'form': form})
+
+
+def update_blog(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            redirect('/')
+    else:
+        form = BlogForm(instance=post)
+    return render(request, 'blog_form.html', {'form': form})
+
+
+def remove_post(request, pk):
+    blog = Post.objects.get(pk=pk)
+    blog.delete()
+    redirect('/')
