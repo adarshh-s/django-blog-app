5e5fced2582b5864b4a36c1514ca7925ce25b04d
Adarsh <adarshs8400@gmail.com>
2026-03-31 13:08:35 +0530

    Created Blog Form

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.list_blogs, name='blog_list'),
-    path('/blog-detail/<int:pk>', views.blog_detail, name='blog_detail')
+    path('/blog-detail/<int:pk>', views.blog_detail, name='blog_detail'),
+    path('/create-blog', views.blog_create, name='blog_create'),
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import BlogForm
 
 # Create your views here.
 
@@ -12,3 +13,13 @@ def list_blogs(request):
 def blog_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog_details.html', {'blog': post})
+
+
+def blog_create(request):
+    form = BlogForm()
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    return render(request, 'blog_form.html', {'form': form})
